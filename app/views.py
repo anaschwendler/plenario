4ed5a8558e3d3b275f2fe00d44c6300e8afdd658
Ana Schwendler <schwendler@gmail.com>
2017-08-19 22:34:07 +0300

    Adds specific page for Senator and Proposition

--- app/views.py
+++ app/views.py
@@ -12,11 +12,27 @@ def senators():
     senators = Senator.query.all()
     return render_template("senators.html", title='Senadores', senators=senators)
 
+@app.route('/senator/<senator_name>')
+def senator_page(senator_name):
+    senator = Senator.query.filter_by(name=senator_name).first()
+    if senator is None:
+        return abort(404)
+    else:
+        return render_template("senator.html",senator=senator)
+
 @app.route('/propositions')
 def propositions():
     propositions = Proposition.query.all()
     votes = Vote.query.all()
     return render_template("propositions.html", title='Proposições', propositions=propositions, votes=votes)
+
+@app.route('/proposition/<proposition_name>')
+def proposition_page(proposition_name):
+    proposition = Proposition.query.filter_by(name=proposition_name).first()
+    if proposition is None:
+        return abort(404)
+    else:
+        return render_template("proposition.html",proposition=proposition)
 
 @app.route('/about')
 def about():
